42c3299b3b157d8495de0e89f6766c42664e23fc
“Zahed75” <zahedhasan.cs@gmail.com>
2025-01-11 12:40:54 +0600

    layout done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { Component } from '@angular/core';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { ProjectComponent } from './pages/project/project.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'layout',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path:'employee',
+        component: EmployeeComponent
+      },
+      {
+        path:'project',
+        component:ProjectComponent
+      }
+    ],
+  },
+];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  loginObj: any = {
+    userName: '',
+    password: '',
+  };
+  http = inject(HttpClient);
+  router = inject(Router);
+  onLogin() {
+    this.http
+      .post(
+        'https://projectapi.gerasim.in/api/EmployeeManagement/login',
+        this.loginObj
+      )
+      .subscribe((res: any) => {
+        if (res.result) {
+          localStorage.setItem('employeeApp', JSON.stringify(res.data));
+          this.router.navigateByUrl('/layout/dashboard');
+        }
+        else{
+          alert(res.message);
+        }
+      });
+  }
 }
