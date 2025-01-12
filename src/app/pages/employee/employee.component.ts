957c13ac54c4ff44e98f5ca7b475250b7617c9fc
“Zahed75” <zahedhasan.cs@gmail.com>
2025-01-13 02:47:17 +0600

    getParentDept API fetched

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,22 +1,32 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { MasterService } from '../../services/master.service';
-import { IApiResponse, IParentDept } from '../../model/Employee';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {MasterService} from '../../services/master.service';
+import {IApiResponse, IParentDept} from '../../model/Employee';
+import {RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 
 @Component({
   selector: 'app-employee',
   imports: [
-
-
+    FormsModule
   ],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent implements OnInit  {
+export class EmployeeComponent implements OnInit {
+
+  parentDeptList: IParentDept[] = [];
+
+  masterService = inject(MasterService);
 
   ngOnInit(): void {
+    this.getParentDeptList()
+  }
 
+  getParentDeptList() {
+    this.masterService.getParentDept().subscribe((res: IApiResponse) => {
+      this.parentDeptList=res.data
+    })
   }
 
 
